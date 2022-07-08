b206b4903f301c9fbcb8f04a63325ad845679f4e
dainiuxt <dainiuxt@gmail.com>
2022-07-08 16:35:26 +0300

    ready for deploy

--- tracker/settings.py
+++ tracker/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = str(os.getenv('SECRET_KEY'))
 # SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['0.0.0.0',
                  'dtutracker.herokuapp.com']
--- tracker/urls.py
+++ tracker/urls.py
@@ -16,10 +16,12 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path, include
+from django.shortcuts import redirect
 
 # from tracker import issues
 
 urlpatterns = [
+    path('', lambda req: redirect('/tracker/')),
     path('issues/', include('issues.urls')),
     path('admin/', admin.site.urls),
 ]
